a2efd42f10419042468a6c932974c25d82b09f6d
Harshal Sonawane <harshalsonawane2025@gmail.com>
2026-02-11 05:05:39 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 // Replace with your actual Firebase project configuration
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  projectId: "studio-7312522036-cf4a5",
+  appId: "1:1038119630074:web:89da66591f76443996c543",
+  apiKey: "AIzaSyCLgx16dzSog05k1adwFx3IavPaQ8yy0uo",
+  authDomain: "studio-7312522036-cf4a5.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "1038119630074",
+  storageBucket: "studio-7312522036-cf4a5.appspot.com"
 };
